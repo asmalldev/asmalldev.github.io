d6414f937f823db78554a4c7ebf944b9b7e08874
asmalldev <asmalldev@gmail.com>
2015-04-10 11:09:05 -0400

    make footer fade in animation less laggy feeling

--- assets/javascript/scrollshow.js
+++ assets/javascript/scrollshow.js
@@ -2,7 +2,7 @@
 
 $("#footer").css('display','none');
 $(window).scroll(function() {
-    if($(window).scrollTop() + 160 > $(document).height() - $(window).height() ) {
+    if($(window).scrollTop() + 180 > $(document).height() - $(window).height() ) {
         $("#footer").fadeIn("fast");
     }
     else {
