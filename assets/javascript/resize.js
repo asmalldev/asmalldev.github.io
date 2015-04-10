d846f94c614099a6ed35a826db4e023b8df9b3a7
asmalldev <asmalldev@gmail.com>
2015-04-10 13:53:52 -0400

    updated resize script for overlay and reenabled

--- assets/javascript/resize.js
+++ assets/javascript/resize.js
@@ -2,8 +2,10 @@
 		var h = $(window).height();
 		var w = $(window).width();
 		var r = h * (w * .75);
+		document.querySelector('#profile_wrapper').style.height = (r*.0009) + 'px';
 		document.querySelector('.container_title').style.height = (r*.00068) + 'px';
-		document.querySelector('#whitebox').style.height = document.querySelector('.container_title').style.height * .5 + 'px';
+		document.querySelector('#white_box').style.height = (r*.00068) + 'px';
+        document.querySelector('.image_overlay').style['background-size'] = 'auto ' + (h+200);
     }
 	window.onresize = resize;
 	resize();
