9719113806208e8c0290646c193e3f224de12e23
asmalldev <asmalldev@gmail.com>
2015-04-10 00:08:47 -0400

    fix resize.js for title container, hotfix!

--- assets/javascript/resize.js
+++ assets/javascript/resize.js
@@ -1,13 +1,10 @@
-<!--
-
 	function resize() {
 		var h = $(window).height();
 		var w = $(window).width();
 		var r = h * (w * .75);
 		document.querySelector('.container_title').style.height = (r*.00068) + 'px';
 		document.querySelector('#whitebox').style.height = document.querySelector('.container_title').style.height * .5 + 'px';
-
+    }
 	window.onresize = resize;
 	resize();
-			
--->
+    
