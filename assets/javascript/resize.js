e44c2f09db54de1e81baf3a3ea380c98bc17a016
asmalldev <asmalldev@gmail.com>
2015-04-09 23:37:56 -0400

    adjust resize script to include white box

--- assets/javascript/resize.js
+++ assets/javascript/resize.js
@@ -5,7 +5,7 @@
 		var w = $(window).width();
 		var r = h * (w * .75);
 		document.querySelector('.container_title').style.height = (r*.00068) + 'px';
-	}
+		document.querySelector('#whitebox').style.height = document.querySelector('.container_title').style.height * .5 + 'px';
 
 	window.onresize = resize;
 	resize();
