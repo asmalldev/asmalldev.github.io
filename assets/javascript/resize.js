804240f9bf91f498206a894d777f134365ba57fb
asmalldev <asmalldev@gmail.com>
2015-04-25 18:27:52 -0400

    remove overlay resize

--- assets/javascript/resize.js
+++ assets/javascript/resize.js
@@ -5,8 +5,6 @@
 		document.querySelector('#profile_wrapper').style.height = (r*.0009) + 'px';
 		document.querySelector('.container_title').style.height = (r*.00068) + 'px';
 		document.querySelector('#white_box').style.height = (r*.00068) + 'px';
-        document.querySelector('.image_overlay').style['background-size'] = 'auto ' + (h+200);
     }
 	window.onresize = resize;
 	resize();
-    
