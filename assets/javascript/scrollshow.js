5db0558a875093e010c990c9973280caf538e2a2
asmalldev <asmalldev@gmail.com>
2016-01-18 01:39:08 -0500

    hide menu on scroll

--- assets/javascript/scrollshow.js
+++ assets/javascript/scrollshow.js
@@ -1,6 +1,6 @@
 <!--
 // show footer on scroll
-$("#footer").css('display','none');
+/*$("#footer").css('display','none');
 $(window).scroll(function() {
     if($(window).scrollTop() + 180 > $(document).height() - $(window).height() ) {
         $("#footer").fadeIn("fast");
@@ -9,5 +9,18 @@ $(window).scroll(function() {
        $("#footer").css('display','none');
     }
 });
+*/
 
+// hide menu on scroll
+$("#menu").css('opacity','1');
+$(window).scroll(function() {
+    $("#menu").css('opacity','1');
+    if($(window).scrollTop() + 4200 > $(document).height() - $(window).height() ) {
+	//$("#menu").fadeOut("fast");
+	$("#menu").css('opacity','0');
+    }
+    else {
+	$("#menu").css('opacity','1');
+    }
+});
 -->
