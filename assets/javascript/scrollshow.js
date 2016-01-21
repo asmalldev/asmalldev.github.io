0a3a2d490eb92baec58291205e12d48f3e6ed8f0
asmalldev <asmalldev@gmail.com>
2016-01-21 01:54:05 -0500

    fix and improve hiding of menubar

--- assets/javascript/scrollshow.js
+++ assets/javascript/scrollshow.js
@@ -1,26 +1,25 @@
 <!--
-// show footer on scroll
-/*$("#footer").css('display','none');
-$(window).scroll(function() {
-    if($(window).scrollTop() + 180 > $(document).height() - $(window).height() ) {
-        $("#footer").fadeIn("fast");
-    }
-    else {
-       $("#footer").css('display','none');
-    }
-});
-*/
-
 // hide menu on scroll
-$("#menu").css('opacity','1');
 $(window).scroll(function() {
     $("#menu").css('opacity','1');
-    if($(window).scrollTop() + 4200 > $(document).height() - $(window).height() ) {
+    var ScrollTop = parseInt($(window).scrollTop());
+    if(ScrollTop > 100) {
 	//$("#menu").fadeOut("fast");
 	$("#menu").css('opacity','0');
     }
     else {
+	//$("#menu").fadeIn("fast");
 	$("#menu").css('opacity','1');
+    }
+});
+
+$(window).scroll(function() {
+    var ScrollTop = parseInt($(window).scrollTop());
+    if(ScrollTop > 500) {
+	$("#menu").css('display','none');
+    }
+    else {
+	$("#menu").css('display','block');
     }
 });
 -->
