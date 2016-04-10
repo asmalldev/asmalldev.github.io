9b48939e9da70b2363da6dc1891714b7cd8e4e1b
asmalldev <asmalldev@gmail.com>
2016-04-10 19:07:30 -0400

    simplify resize.js and improve profile design; fix margin of back to top button

--- assets/javascript/resize.js
+++ assets/javascript/resize.js
@@ -1,11 +1,8 @@
 <!--
-// resize profile wrapper, container_title and white_box according to window
+// resize container_title according to window
 function resize() {
 	var h = $(window).height();
-	var w = $(window).width();
-	var r = h * (w * .35);
-	document.querySelector('#profile_wrapper').style.height = (r*.0009) + 'px';
-	document.querySelector('.container_title').style.height = (r*.00068) + 'px';
+	document.querySelector('.container_title').style.height = h + 'px';
 }
 window.onresize = resize;
 resize();
