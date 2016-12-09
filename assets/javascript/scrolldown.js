d2e00f2b32366b4f6aab10afdb5c615b5d2a0b2a
Austin Small <asmalldev@gmail.com>
2016-12-09 13:45:13 -0500

    adjust scroll down script for small screens

--- assets/javascript/scrolldown.js
+++ assets/javascript/scrolldown.js
@@ -6,8 +6,16 @@ $("#CVscroll").click(function() {
 		475);
 });
 $("#scroll_down").click(function() {
-	$('html,body').animate({
-		scrollTop: $(".rest_wrapper").offset().top},
-		475);
+	var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+	if (width > 550) {
+		$('html,body').animate({
+			scrollTop: $(".rest_wrapper").offset().top},
+			475);
+	}
+	else {
+		$('html,body').animate({
+			scrollTop: $(".container").offset().top},
+			475);
+	}
 });
 -->
