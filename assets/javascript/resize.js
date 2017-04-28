6c14ca3eb3bb9b9164d3f369b62890939d47306b
Austin Small <asmalldev@gmail.com>
2017-04-28 16:21:24 -0400

    return of the image overlay, feat. Boston

--- assets/javascript/resize.js
+++ assets/javascript/resize.js
@@ -1,7 +1,7 @@
 <!--
 // resize container_title according to window
 function resize() {
-	var h = $(window).height() - 50;
+	var h = $(window).height() + 68;
 	document.querySelector('.container_title').style.height = h + 'px';
 }
 window.onresize = resize;
--- assets/javascript/scrollshow.js
+++ assets/javascript/scrollshow.js
@@ -4,20 +4,21 @@ $(window).scroll(function() {
     $("#menu").css('opacity','1');
     var ScrollTop = parseInt($(window).scrollTop());
     if(ScrollTop > 100) {
-	$("#menu").css('margin-top','-4em');
+	       $("#menu").css('margin-top','-4em');
     }
     else {
-	$("#menu").css('margin-top','0');
+	       $("#menu").css('margin-top','0');
     }
 });
 
+// show footer
 $(window).scroll(function() {
     var ScrollTop = parseInt($(window).scrollTop());
-    if(ScrollTop > 500) {
-	$("#menu").css('display','none');
+    if(ScrollTop > 2000) {
+        $("#footer").css('display', 'block');
     }
     else {
-	$("#menu").css('display','block');
+        $("#footer").css('display', 'none');
     }
 });
 -->
