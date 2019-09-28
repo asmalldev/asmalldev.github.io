2f373f774fa1226718f54622035c49b7f57c6d15
Austin Small <austin@focal.systems>
2019-09-28 09:42:50 -0700

    remove stuff, choose dark colors

--- assets/javascript/resize.js
+++ assets/javascript/resize.js
@@ -1,13 +1,17 @@
 <!--
-// resize container_title according to window
+// resize containers according to window size
+var elements = [".container_title"];
+
 function resize() {
 	var w = $(window).width();
 	var h = $(window).height();
-	if (w < 535) {
-		document.querySelector('.container_title').style.height = (h - 68) + 'px';
-	} else {
-		document.querySelector('.container_title').style.height = (h + 68) + 'px';
-	}
+    for (var i = 0; i < elements.length; i++) {
+        if (w < 535) {
+            document.querySelector(elements[i]).style.height = (h - 68) + 'px';
+        } else {
+            document.querySelector(elements[i]).style.height = (h + 68) + 'px';
+        }
+    }
 }
 window.onresize = resize;
 resize();
