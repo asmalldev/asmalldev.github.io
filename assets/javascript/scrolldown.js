70e789075792bea4ad2cbc7538d6c1e4a252afcc
Austin Small <austin@focal.systems>
2020-04-05 00:26:01 -0700

    content + style update

--- assets/javascript/scrolldown.js
+++ assets/javascript/scrolldown.js
@@ -7,15 +7,8 @@ $(".CVscroll").click(function() {
 });
 $("#scroll_down").click(function() {
 	var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-	if (width > 550) {
-		$('html,body').animate({
-			scrollTop: $(".rest_wrapper").offset().top},
-			475);
-	}
-	else {
-		$('html,body').animate({
-			scrollTop: $(".container").offset().top},
-			475);
-	}
+    $('html,body').animate({
+        scrollTop: $(".rest_wrapper").offset().top},
+        475);
 });
 -->
